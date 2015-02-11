592cb532fda50dd6c38cc25b201da2fd3d85b346
ajustice23 <ajustice23@gmail.com>
2015-02-11 10:40:42 -0800

    new Deck class

--- Deck.java
+++ Deck.java
@@ -2,37 +2,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
-
 	ArrayList<Card> deck = new ArrayList<Card>();
+	int i=0;
 
-	public void makeDeck() {
+	public Deck(){
+		//ArrayList<Card> deck = new ArrayList<Card>();
 		for (int i = 0; i < 4; i++) {
 			for (int j = 1; j < 14; j++) {
 				Card tempCard = new Card(i, j);
 				deck.add(tempCard);
 			}
 		}
-		shuffleDeck();
+		Collections.shuffle(deck);	
+
 		System.out.println(deck.size());
-
-		//System.out.println(deck);
 	}
 
-	public void shuffleDeck() {
-		Collections.shuffle(deck);
-	}
 
 	public Card drawCard() {
-		Card cardDrawn = deck.get(0);
-		deck.remove(0);
-		System.out.println(deck.size());
+		
+		Card cardDrawn = deck.get(i);
+		i++;
+		System.out.println(52-i);
 		return cardDrawn;
 
-	}
-
-	public static void main(String[] arg) {
-		Deck tryDeck = new Deck();
-		tryDeck.makeDeck();
-		tryDeck.drawCard();
 	}
 }
